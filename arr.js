eed7d478894831f1ba055a356241a4f9b55970ca
supriya224 <52038704+supriya224@users.noreply.github.com>
2023-11-27 16:02:58 +0530

    commit files

---
+++ arr.js
@@ -0,0 +1,10 @@
+const arr = [10, 12, 14, 18, 21, 24, 27, 30];
+const ans=25;
+let temp;
+for(let i=0; i<arr.length; i++){
+    {
+       if(arr[i]<ans) 
+       temp = arr[i] 
+    }
+} 
+console.log(temp);
