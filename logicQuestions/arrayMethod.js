93e5b51575795eb687d49dc2608f4c6caed4cca6
supriya224 <52038704+supriya224@users.noreply.github.com>
2023-11-30 12:02:23 +0530

    first commit

---
+++ logicQuestions/arrayMethod.js
@@ -0,0 +1,27 @@
+
+// Array length
+// var data =[23,1,54,67,9,1,3,455,7];
+//     console.log(data.length);
+
+// Array shift 
+var ele =[20,30,40,50,60];  //remove element in an array to front
+console.log(ele.shift());
+console.log(ele);
+
+// array unshift
+var elem =[10,20,30,40]; //add element in an array to the front
+console.log(elem.unshift(50));
+console.log(elem);
+
+// pop
+var val =[10,20,30,40,50]; //it use for delete element end of an array
+console.log(val.pop());
+console.log(val)
+
+// push
+var pus =[10,20,30,40]; //add element of the end of the array
+console.log(pus.push(50));
+console.log(pus) 
+
+// indexOf
+
--- logicQuestions/factorialES6.js
+++ logicQuestions/factorialES6.js
@@ -1,2 +1,2 @@
-const fac = (n) => (n === 0 ? 1 : n * fac(n - 1));  // esme agr n ya to 0 ke ya to 1 ke qual ni hai to to : eske bad wala code run hoyega
+const fac = (n) => (n === 0 ? 1 : n * fac(n - 1));  // esme agr n ya to 0 ke ya to 1 ke qual ni hai to to : eske bad wala code run hoga
 console.log(fac(5));
