db33a4def4457786b9d82d9fc725cc80556a1fca
supriya224 <52038704+supriya224@users.noreply.github.com>
2023-12-06 21:59:43 +0530

    commit file

---
+++ logicQuestions2/replace.js
@@ -0,0 +1,12 @@
+var text =" i want to become frontend developer "
+let te= "frontend"
+console.log(text)
+console.log(text.replace(te, "web"));
+
+// replaceAll
+
+var patt =" i want to become website developer website is the collection of web pages"
+var pat ="website"
+var pat =/website/g // global replacement
+console.log(patt)
+console.log(patt.replaceAll(pat, "application"))
