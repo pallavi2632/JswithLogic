a166ffd41f7ba15098c353b8b64e065b404718fa
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-01-27 23:18:39 +0530

    commit changes

---
+++ logicQuestions3/swap.js
@@ -0,0 +1,19 @@
+ // without third variable
+
+let a = 18;
+let b = 3;
+[a,b]=[b, a];
+console.log(` value of a is ${a} and b is ${b}`);
+
+
+
+
+  // with third variable
+  x=34;
+  y=90
+  let temp1 = x
+  x=y
+  y=temp1
+  console.log(` value of a is${x} and b is ${y}`)
+
+ 
